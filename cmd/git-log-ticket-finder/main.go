cf2f4dac350866adfdaea8970a8f91f7ffe1c865
Guillaume Rivest <guillaume.rivest@desjardins.com>
2019-10-22 12:46:38 -0400

    Initial, working version of the Go code itself

--- cmd/git-log-ticket-finder/main.go
+++ cmd/git-log-ticket-finder/main.go
@@ -1,4 +1,70 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+)
+
+var TicketSlice []string
+
 func main() {
+	ticketRegex := flag.String("tickets", "", "")
+	content := flag.String("content", "", "")
+
+	flag.Parse()
+
+	if ticketRegex == nil || *ticketRegex == "" {
+		log.Fatal("Missing parameter '--tickets'")
+	}
+
+	if content == nil || *content == "" {
+		log.Fatal("Missing parameter '--content'")
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(*content))
+	for scanner.Scan() {
+		if present, ticket := findTickets(scanner.Text(), *ticketRegex); present {
+			TicketSlice = append(TicketSlice, ticket...)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("reading standard input: %v", err)
+	}
+
+	TicketSlice = ensureUniqueValues(TicketSlice)
+
+	fmt.Println(TicketSlice)
+}
+
+func findTickets(text, ticketRegex string) (bool, []string) {
+	regex := "((?:"
+	regex += strings.ReplaceAll(ticketRegex, ",", "|")
+	regex += ")-[0-9]+)"
+
+	r, _ := regexp.Compile(regex)
+
+	out := r.FindAllString(text, -1)
+
+	if len(out) == 0 {
+		return false, []string{}
+	}
+
+	return true, out
+}
+
+func ensureUniqueValues(s []string) []string {
+	keys := make(map[string]bool)
+	var list []string
+	for _, entry := range s {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+
+	return list
 }
